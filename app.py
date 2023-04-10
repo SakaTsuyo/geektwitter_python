98018cc56a80e0d275c11012655a9a8d5c04a1c6
つよぽん <tsuyopon@gmail.com>
2023-04-10 12:16:46 +0900

    20220409

--- app.py
+++ app.py
@@ -3,19 +3,34 @@ import numpy as np
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class Post(db.Model):
+class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20), nullable=False)
     body = db.Column(db.String(140), nullable=False)
     
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(25))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 @app.route('/')
 def index():
-    posts = Post.query.all()
+    posts = Tweet.query.all()
     return render_template("index.html", posts = posts)
 
 @app.route('/new', methods=['GET','POST'])
@@ -23,7 +38,7 @@ def create():
     if request.method == 'POST':
         title = request.form.get('title')
         body = request.form.get('body')
-        post = Post(title=title,body=body)
+        post = Tweet(title=title,body=body)
         db.session.add(post)
         db.session.commit()
         return redirect('/')
@@ -32,7 +47,7 @@ def create():
 
 @app.route('/<int:id>/edit',methods=['GET','POST'])
 def edit(id):
-    post = Post.query.get(id)
+    post = Tweet.query.get(id)
     if request.method == 'GET':
         return render_template('edit.html', post=post)
     else:
@@ -43,10 +58,40 @@ def edit(id):
 
 @app.route('/<int:id>/delete', methods=['GET'])
 def delete(id):
-    post = Post.query.get(id)
+    post = Tweet.query.get(id)
     db.session.delete(post)
     db.session.commit()
     return redirect('/')
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect('login')
+    else:
+        return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/tweets')
+        else:
+            return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('login')
 
 if __name__ == "__main__":
     # Post.create_tweet_table()
